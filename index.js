4efe7b53d443aa51f2674e745590b4eb63bf3790
Prottoy16 <prottoybiswas16@gmail.com>
2023-11-27 21:13:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var para=document.querySelector("#para");
+para.addEventListener("mouseover",function(){
+    para.classList.add("para");
+});
+
+para.addEventListener("mouseout",function(){
+    para.classList.remove("para");
+    
+});
